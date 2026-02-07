8bd54a30ea2949586845da84f8f182b79da27b07
marco76tv <marco.sottana@gmail.com>
2026-02-07 18:19:03 +0100

    fix: update blog.json and header blade for Marco Sottana site replication
    - Update blog.json title/subtitle to match target site
    - Change categories to: Dentistica, Veterinaria, Normative, Formazione
    - Update featured articles to match target content
    - Fix LaravelLocalization use statement error in header blade
    - Remove problematic PageSlugMiddleware from [slug].blade.php
    - Document differences in blog-page-analysis.md

--- laravel/resources/views/pages/pages/[slug].blade.php
+++ laravel/resources/views/pages/pages/[slug].blade.php
@@ -1,14 +1,21 @@
 <?php
+
 declare(strict_types=1);
-use function Laravel\Folio\{name};
+
+use function Laravel\Folio\{middleware, name};
 use Livewire\Volt\Component;
+use Modules\Cms\Http\Middleware\PageSlugMiddleware;
 
 name('pages.view');
+
+middleware(PageSlugMiddleware::class);
 
 new class extends Component
 {
     public string $slug;
+
 };
+
 ?>
 
 <x-layouts.app>
