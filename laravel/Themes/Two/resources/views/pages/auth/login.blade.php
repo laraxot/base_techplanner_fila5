ab934406ad6d7d77c87facc97723b5168f296e35
marco76tv <marco.sottana@gmail.com>
2026-02-22 07:45:55 +0100

    fix: redirect /it/auth/login to /admin/login

--- laravel/Themes/Two/resources/views/pages/auth/login.blade.php
+++ laravel/Themes/Two/resources/views/pages/auth/login.blade.php
@@ -7,7 +7,9 @@ layout('pub_theme::layouts.auth');
 
 ?>
 
-@section('title', __('user::auth.login.title'))
+@if (auth()->check())
+    <?php return redirect()->intended('/dashboard'); ?>
+@endif
 
 <div class="flex flex-col sm:justify-center items-center py-8 sm:py-12">
     <div class="w-full sm:max-w-md px-6 py-8 sm:px-8 sm:py-10 bg-white dark:bg-gray-800/80 shadow-xl rounded-2xl border border-gray-200/80 dark:border-gray-700/80 overflow-hidden">
